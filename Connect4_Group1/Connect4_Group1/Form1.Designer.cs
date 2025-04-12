38f9a57d99671573c07d5188c782cc55be81be50
CecildeSist <iyounglove@my.monroeccc.edu>
2025-04-11 23:43:11 -0400

    Start Form Beautification
    Edited visuals of the starting form so it's easier on the eyes

--- Connect4_Group1/Connect4_Group1/Form1.Designer.cs
+++ Connect4_Group1/Connect4_Group1/Form1.Designer.cs
@@ -38,64 +38,74 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.BackColor = Color.Red;
-            label1.Font = new Font("Hollywood Hills", 72F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label1.Location = new Point(125, 59);
+            label1.BackColor = Color.Firebrick;
+            label1.Font = new Font("Matura MT Script Capitals", 36F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Black;
+            label1.Location = new Point(51, 9);
             label1.Name = "label1";
-            label1.Size = new Size(532, 172);
+            label1.Size = new Size(694, 64);
             label1.TabIndex = 0;
-            label1.Text = "Welcome to \r\nConnect 4";
+            label1.Text = "Ye Olde Connect Four Game";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // btn_Singleplayer
             // 
-            btn_Singleplayer.Font = new Font("Hollywood Hills", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            btn_Singleplayer.Location = new Point(36, 318);
+            btn_Singleplayer.BackColor = Color.Silver;
+            btn_Singleplayer.Font = new Font("Viner Hand ITC", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btn_Singleplayer.ForeColor = Color.MediumBlue;
+            btn_Singleplayer.Location = new Point(51, 76);
             btn_Singleplayer.Name = "btn_Singleplayer";
             btn_Singleplayer.Size = new Size(158, 81);
             btn_Singleplayer.TabIndex = 1;
-            btn_Singleplayer.Text = "Singleplayer";
-            btn_Singleplayer.UseVisualStyleBackColor = true;
+            btn_Singleplayer.Text = "Single Player";
+            btn_Singleplayer.UseVisualStyleBackColor = false;
             btn_Singleplayer.Click += btn_Singleplayer_Click;
             // 
             // btn_Twoplayer
             // 
-            btn_Twoplayer.Font = new Font("Hollywood Hills", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            btn_Twoplayer.Location = new Point(210, 318);
+            btn_Twoplayer.BackColor = Color.Silver;
+            btn_Twoplayer.Font = new Font("Viner Hand ITC", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btn_Twoplayer.ForeColor = Color.MediumBlue;
+            btn_Twoplayer.Location = new Point(215, 76);
             btn_Twoplayer.Name = "btn_Twoplayer";
             btn_Twoplayer.Size = new Size(158, 81);
             btn_Twoplayer.TabIndex = 2;
-            btn_Twoplayer.Text = "Two Player";
-            btn_Twoplayer.UseVisualStyleBackColor = true;
+            btn_Twoplayer.Text = "Two Players";
+            btn_Twoplayer.UseVisualStyleBackColor = false;
             btn_Twoplayer.Click += btn_Twoplayer_Click;
             // 
             // btn_Exit
             // 
-            btn_Exit.Font = new Font("Hollywood Hills", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            btn_Exit.Location = new Point(587, 318);
+            btn_Exit.BackColor = Color.Silver;
+            btn_Exit.Font = new Font("Viner Hand ITC", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btn_Exit.ForeColor = Color.MediumBlue;
+            btn_Exit.Location = new Point(587, 76);
             btn_Exit.Name = "btn_Exit";
             btn_Exit.Size = new Size(158, 81);
             btn_Exit.TabIndex = 3;
             btn_Exit.Text = "Exit";
-            btn_Exit.UseVisualStyleBackColor = true;
+            btn_Exit.UseVisualStyleBackColor = false;
             btn_Exit.Click += btn_Exit_Click;
             // 
             // btn_Stats
             // 
-            btn_Stats.Font = new Font("Hollywood Hills", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            btn_Stats.Location = new Point(411, 318);
+            btn_Stats.BackColor = Color.Silver;
+            btn_Stats.Font = new Font("Viner Hand ITC", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btn_Stats.ForeColor = Color.MediumBlue;
+            btn_Stats.Location = new Point(423, 76);
             btn_Stats.Name = "btn_Stats";
             btn_Stats.Size = new Size(158, 81);
             btn_Stats.TabIndex = 4;
             btn_Stats.Text = "Statistics";
-            btn_Stats.UseVisualStyleBackColor = true;
+            btn_Stats.UseVisualStyleBackColor = false;
             btn_Stats.Click += btn_Stats_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = SystemColors.MenuHighlight;
-            ClientSize = new Size(800, 450);
+            BackColor = Color.MediumSlateBlue;
+            ClientSize = new Size(800, 171);
             Controls.Add(btn_Stats);
             Controls.Add(btn_Exit);
             Controls.Add(btn_Twoplayer);
